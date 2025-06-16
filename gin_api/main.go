92979fd1f949f51676521cc24ba617e9830c9874
Phoenix Sheppard <phoenixsheppard28@gmail.com>
2025-06-16 16:49:15 -0400

    lol

--- gin_api/main.go
+++ gin_api/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func handle(c *gin.Context) {
+	c.Writer.Write([]byte("Hello world"))
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Could not load environment variables")
+	}
+	router := gin.Default()
+	router.GET("/handle", handle)
+
+	router.Run(os.Getenv("PORT"))
 }
