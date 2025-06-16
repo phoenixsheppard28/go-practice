4b89ab438703a49b0e58df12cdff7a7bdd50e8ee
Phoenix Sheppard <phoenixsheppard28@gmail.com>
2025-06-16 17:06:52 -0400

    works

--- gin_api/main.go
+++ gin_api/main.go
@@ -2,14 +2,67 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
+type Input struct {
+	Num1     *int   `json:"num1" binding:"required"`
+	Num2     *int   `json:"num2" binding:"required"`
+	Operator string `json:"operator" binding:"required"`
+}
+
 func handle(c *gin.Context) {
-	c.Writer.Write([]byte("Hello world"))
+	var inp Input
+	if err := c.ShouldBindBodyWithJSON(&inp); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	num1 := *inp.Num1
+	num2 := *inp.Num2
+	oper := inp.Operator
+
+	var res int
+	switch oper {
+	case "+":
+		res = num1 + num2
+	case "-":
+		res = num1 - num2
+
+	case "/":
+		if num2 == 0 {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "divide by zero",
+			})
+		}
+		res = num1 / num2
+	case "*":
+		res = num1 * num2
+
+	case "%":
+		if num2 == 0 {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "mod by zero",
+			})
+		}
+		res = num1 % num2
+
+	default:
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid operator, only support + - / * %",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"result": res,
+	})
 }
 
 func main() {
@@ -18,7 +71,7 @@ func main() {
 		log.Fatal("Could not load environment variables")
 	}
 	router := gin.Default()
-	router.GET("/handle", handle)
+	router.POST("/handle", handle)
 
 	router.Run(os.Getenv("PORT"))
 }
